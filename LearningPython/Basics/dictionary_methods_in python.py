f17447fa7ab6f8e368628bbbc0ecc86c092cf286
kiran <poluparthi.kirankumar@gmail.com>
2024-02-09 23:31:18 +0530

    committed dictionary methods

---
+++ LearningPython/Basics/dictionary_methods_in python.py
@@ -0,0 +1,15 @@
+d1 = {1: 23, 2: 45, 3: 90, "qa": "test_url", "prod": "prod_url", "UAT": "uat_url",'qs': 56,89: "qwerty"}
+print(d1.get("prod")) # returns the values of particular key that is passed
+print(d1.keys())   # returns only the keys of that dictionary
+print(d1.values())  # returns only the values of that dictionary
+print(d1.items())   # returns copy of the key-value pairs as a list
+print(d1.pop("qa"))  # return the value of that particular key
+print(d1)  # after removing the above , it will print the remaining
+print(d1.popitem()) # returns the last key value pair
+print(d1)  # after removing the above , it will print the remaining
+d1.update({"city":"Bangalore"})  # update the existing dictionary with this key-value pair
+print(d1)
+d1_copy=d1.copy()   # copy the dictionary
+print(d1_copy)
+d1_copy.clear()     # clears the dictionary 
+print(d1_copy)

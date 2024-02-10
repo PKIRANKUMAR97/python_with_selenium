7726833eb05c8eaecdc55ab350f51654537760dd
kiran <poluparthi.kirankumar@gmail.com>
2024-02-10 11:02:53 +0530

    added if else

---
+++ LearningPython/Basics/if_else_in_python.py
@@ -0,0 +1,9 @@
+a = 89
+if a > 90:
+    print("value is greater than 90")
+elif a > 80 and a < 90:
+    print("greater than 80 and less than 90")
+elif a < 80 :
+    print("less than 70")
+else:
+    print("nothing")

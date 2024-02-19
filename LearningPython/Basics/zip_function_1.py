c8c1deceb8ac5f33f292c16b84436f7e9245389d
kiran <poluparthi.kirankumar@gmail.com>
2024-02-19 21:12:06 +0530

    committed zip function

---
+++ LearningPython/Basics/zip_function_1.py
@@ -0,0 +1,34 @@
+# list1=["India","china","Japan","America"]
+# list2=["Hyderabad","Beijing","Tokyo","Newyork"]
+# z=zip(list1,list2)
+# print(list(z))
+
+# list3=["India","china","Japan","America","Pakistan","Germany"]
+# list4=["Hyderabad","Beijing","Tokyo","Newyork"]
+# z1=zip(list3,list4)
+# print(list(z1))
+
+# list5=["India","china","Japan","America"]
+# list6=["Hyderabad","Beijing","Tokyo","Newyork","Pakistan","Germany"]
+# z2=zip(list5,list6)
+# print(list(z2))
+
+# cost_price=[56,90,34,22]
+# selling_price=[40,78,31,78]
+# for x,y in zip(cost_price,selling_price):
+#     print(x,y,x-y)
+
+# cost_price = {56, 90, 34, 22}
+# selling_price = {40, 78, 31, 78}
+# for x, y in zip(cost_price, selling_price):
+#     print(x - y)
+
+# list1 = ["India", "china", "Japan", "America"]
+# list2 = ["Hyderabad", "Beijing", "Tokyo", "Newyork"]
+# for x,y in zip(list1,list2):
+#     print(x,y)
+
+set1 = {"India", "china", "Japan", "America"}
+set2 = {"Hyderabad", "Beijing", "Tokyo", "Newyork"}
+for x,y in zip(set1,set2):
+    print(x,y)

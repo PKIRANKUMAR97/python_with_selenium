48cc4bfbd3088d444948e8ea2635fc9c996282bd
kiran <poluparthi.kirankumar@gmail.com>
2024-02-19 20:42:35 +0530

    committed some example programs

--- LearningPython/Basics/examples_1.py
+++ LearningPython/Basics/examples_1.py
@@ -1,8 +1,18 @@
-a= "Kiran"
-b="SAP"
-print(a+b)
+# a= "Kiran"
+# b="SAP"
+# print(a+b)
+#
+# a="2"
+# b="9"
+# print(a+b)
 
-a="2"
-b="9"
-print(a+b)
-
+# Sum of elements in a list
+# list_1 = [6, 7, 3, 89, 324, 533]
+# sum = 0
+# for i in list_1:
+#     sum = sum + i
+# print(sum)
+# print all  element in a list
+list_2 = [32, 89, 234, 190, 12, 90, 256, 98]
+for i in range(len(list_2)):
+    print(list_2[i])

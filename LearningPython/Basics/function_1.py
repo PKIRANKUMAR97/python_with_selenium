60bebd7fde06b50e39805599d326c2fa0b28dab1
kiran <poluparthi.kirankumar@gmail.com>
2024-02-23 22:01:46 +0530

    committed userdefined functions

--- LearningPython/Basics/function_1.py
+++ LearningPython/Basics/function_1.py
@@ -7,16 +7,24 @@ addition(100, 200, 300)
 
 """
 def add(num1: int, num2: int) -> int:
-    """Add two numbers"""
     num3 = num1 + num2
 
     return num3
 
 
-# Driver code
 num1, num2 = 5, 15
 ans = add(num1, num2)
 print(f"The addition of {num1} and {num2} results {ans}.")
 print("The addition of {num1} and {num2} results {ans}.")
 
 """
+def add(a,b,c):
+    """
+    This is a function for adding 2 numbers
+    :param a: integer a
+    :param b: integer b
+    :return: this returns the sum of 2 numbers
+    """
+    return a+b+c
+z=add(1.231,3000.31,4.0)
+print(z)

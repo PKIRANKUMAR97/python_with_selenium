a586049cc7aab5faf5c854e0c13dc7a43c63b8f8
kiran <poluparthi.kirankumar@gmail.com>
2024-02-24 16:25:22 +0530

    committed classes and objects in python

---
+++ LearningPython/Basics/classes_objects_1.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self, fname, lname,age):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+
+    def complete_details(self):
+        print("my first name is: " + self.fname)
+        print("my last name is: " + self.lname)
+        print("my age is:" + self.age)
+
+
+
+emp1 = Employee("KIRAN", "KUMAR",str(34))
+emp2 = Employee("SAI", "KIRAN",str(67))
+print(emp1.complete_details())
+print(emp2.complete_details())

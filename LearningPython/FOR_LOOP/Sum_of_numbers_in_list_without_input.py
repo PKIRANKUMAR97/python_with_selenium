454a2e6059bf2184d5c24113608fd89f1151a61d
kiran <poluparthi.kirankumar@gmail.com>
2024-03-03 00:57:01 +0530

    committed for_loop_programs folder

---
+++ LearningPython/FOR_LOOP/Sum_of_numbers_in_list_without_input.py
@@ -0,0 +1,6 @@
+list1=[34,61,90,23,84,194,329]
+sum = 0
+for i in list1:
+    sum = sum + i
+
+print(sum)
---
+++ LearningPython/FOR_LOOP/sum_of_numbers_in_list_with_input.py
@@ -0,0 +1,12 @@
+list_2=[]
+len_list=int(input())
+for i in range(0,len_list):
+    list_2.append(int(input()))
+
+print(list_2)
+sum = 0
+for i in list_2:
+    sum = sum + i
+
+
+print("SUM="'%s' % sum)
